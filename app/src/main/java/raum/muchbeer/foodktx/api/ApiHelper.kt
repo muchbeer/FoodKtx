0beff416011daa191c1f8c8fc02b145461b7d2dd
muchbeer <gman2them@gmail.com>
2021-02-04 23:52:58 +0300

    best mvvm retrofit food api app

--- app/src/main/java/raum/muchbeer/foodktx/api/ApiHelper.kt
+++ app/src/main/java/raum/muchbeer/foodktx/api/ApiHelper.kt
@@ -8,20 +8,7 @@ import retrofit2.Response
 
 
 class ApiHelper( private val foodService: FoodService) {
-    companion object {
-        private const val API_KEY = "39be4261677f46598022e57a1ca64c4a"
-    }
+
     suspend fun getFood() : Response<FoodModel> = foodService.getFood(BuildConfig.API_KEY, true)
 
-/*{
-        lateinit var listOfFood : List<Food>
-
-        try {
-            listOfFood = foodService.getFood(BuildConfig.API_KEY, true).body()!!.results
-            Log.d("MovieNetwork", "retrieve from network: ${listOfFood}")
-        } catch (exception: Exception) {
-
-        }
-
-    }*/
 }
